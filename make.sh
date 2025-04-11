61e7461a9181f1d3d57c90ad612d8315e3b76045
Ethan <EthanGrandfield@my.unt.edu>
2025-04-10 22:17:57 -0500

    Cleaned up make.sh

--- make.sh
+++ make.sh
@@ -9,4 +9,10 @@ aarch64-linux-gnu-gcc -o pa3 pa3.o -nostartfiles
 sleep 2
 
 echo "Running..."
+echo ""
 qemu-aarch64 -L /usr/aarch64-linux-gnu ./pa3
+
+echo ""
+echo "Cleaning up..."
+rm -f pa3.o pa3
+echo "Cleanup complete."
